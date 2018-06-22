dcdc6f6a9e2ac22d2c882f131f96a39867681c2c
Shubham Agarwal <shubhamagarwal3010@gmail.com>
2018-06-22 11:51:39 +0530

    added splash screen

---
+++ app/src/main/java/com/iot/letthingsspeak/common/views/ActivitySplashScreen.java
@@ -0,0 +1,64 @@
+/*===============================================================================
+Copyright (c) 2016 PTC Inc. All Rights Reserved.
+
+Copyright (c) 2012-2014 Qualcomm Connected Experiences, Inc. All Rights Reserved.
+
+Vuforia is a trademark of PTC Inc., registered in the United States and other 
+countries.
+===============================================================================*/
+
+package com.iot.letthingsspeak.common.views;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.ViewGroup.LayoutParams;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.RelativeLayout;
+
+import com.iot.letthingsspeak.R;
+import com.iot.letthingsspeak.identity.views.LoginActivity;
+
+
+public class ActivitySplashScreen extends Activity
+{
+    
+    private static long SPLASH_MILLIS = 450;
+    
+    
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        
+        RelativeLayout layout = (RelativeLayout) View.inflate(
+            this, R.layout.activity_splash_screen, null);
+        
+        addContentView(layout, new LayoutParams(LayoutParams.MATCH_PARENT,
+            LayoutParams.MATCH_PARENT));
+        
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable()
+        {
+            
+            @Override
+            public void run()
+            {
+                
+                Intent intent = new Intent(ActivitySplashScreen.this,
+                    LoginActivity.class);
+                startActivity(intent);
+                
+            }
+            
+        }, SPLASH_MILLIS);
+    }
+    
+}
